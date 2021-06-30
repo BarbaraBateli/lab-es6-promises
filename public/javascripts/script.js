db8e3bb7ea8b10609b1232a85c0923317102c535
Barbara Bateli <barbarabateli@MacBook-Air-de-Barbara.local>
2021-06-30 15:37:41 -0300

    done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,7 +2,7 @@
 // we added it for you to test to make sure data is loaded
 // 🚨🚨🚨 comment out the next 3 lines when you start working on the code
 for (let i = 0; i < mashPotatoes.length; i++) {
-  //addFood(steak[i], '#steak');
+  addFood(steak[i], '#steak');
   console.log(mashPotatoes[i])
 }
 
@@ -17,7 +17,7 @@ const cookSteak = new Promise((resolve,reject) => {
                   addFood(steak[7],'#steak',() => {
                   document.querySelector(
                   '#table'
-                  ).innerHTML += `<img src="../images/steak.jpg"/>`;
+                  ).innerHTML += `<img src="./public/images/steak.jpg"/>`;
                   resolve();
                 });
               });
@@ -25,7 +25,7 @@ const cookSteak = new Promise((resolve,reject) => {
           });
         });
       });
-    }):
+    });
   });
 });
 
@@ -35,14 +35,13 @@ const cookSteak = new Promise((resolve,reject) => {
 // Iteration 2 using `.then()`
 const cookMash = new Promise((resolve, reject) => {
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
-  addFood(mashPotatoes[1], '#mashPotatoes').then(()
-    => {
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
       addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
         addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
           addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
             document.querySelector(
               '#table'
-            ).innerHTML += `<img src="../images/mashPotatoes.jpg"/>`;
+            ).innerHTML += `<img src="./public/images/mashPotatoes.jpg"/>`;
             resolve();
           });
         });
@@ -53,8 +52,38 @@ addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
 
 // Iteration 3 using async/await
 
-  async function makeFood(step) {
-    // ... your code here
-    
+  async function makeFood(steps, id) {
+for (let step of steps) {
+  await addFood(step, id);
+}
+return new Promise(async (resolve, reject) => {
+  for (let step of steps) {
+    await addFood(step, id);
   }
-  makeFood(eachStep);
+
+document.querySelector(
+  "#table"
+).innerHTML += `<img src="./public/images/brusselSprouts.jpg"/>`;
+
+document.querySelector(
+  "#table"
+).innerHTML += `<img src="./public/images/${id.replace("#", " ")}.jpg"/>`;
+    resolve();
+  });
+}
+ 
+(async function run() {
+  await Promise.all([
+    makeFood(steak, "#steak"),
+    makeFood(mashPotatoes, "#mashPotatoes"),
+    makeFood(brusselSprouts, "#brusselSprouts"),
+  ]);
+const cookBrusselSprouts = makeFood(brusselSprouts, "#brusselSprouts");
+
+Promise.all([cookBrusselSprouts, cookSteak, cookMash]).then((result) => {
+  const audio = new Audio("./public/media/dinnerIsServed.mp3");
+  audio.volume = 0.5;
+  audio.play();
+  alert("Dinner is served!");
+});
+});
